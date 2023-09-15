7c2478fe90ab5952af14ec6b6205dd2cc5a27e1d
TkDevk <duvanuriel@hotmail.com>
2023-09-14 22:31:50 -0500

    Como pensaba que funcionaban las funciones

---
+++ numberOne/script.js
@@ -0,0 +1,46 @@
+/*
+
+Reto del día : Hacer un programa para un restaurante donde el usuario pueda elegir entre 6 platillos distintos, cada platillo con un precio distinto y que cada vez que el usuario seleccione sun platillo se le agregue a la orden y se sumen los precios
+
+*/
+
+//Elegir platos[distintos]
+//Cada platillo tiene un precio distinto
+//El usuario agrega (button) a la orden y se sumen
+
+
+//Declaro funcion
+//Funcion es como crear una accion
+
+    
+function mealOne() {
+    const priceOne = 300;
+    console.log(priceOne);
+}
+ function mealTwo() {
+    const priceTwo = 400;
+    console.log(priceTwo);
+}
+function mealThree() {
+    const priceThree = 500;
+    console.log(priceThree);
+}
+function mealFour() {
+    const priceFour = 600;
+    console.log(priceFour);
+}
+function mealFive() {
+    const priceFive = 700;
+    console.log(priceFive);
+}
+function mealSix() {
+    const priceSix = 800;
+    console.log(priceSix);
+    return priceSix;
+}
+
+function addMeal(){
+    let resultado = priceFive+priceFour;
+    console.log(resultado);
+}
+
